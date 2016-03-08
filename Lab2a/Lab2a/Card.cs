517539112875adcdbf386fd260a813c0f38a7b07
Rikard92 <Rikard.Persson92@hotmail.com>
2016-03-08 10:53:05 +0100

    Add klass

---
+++ Lab2a/Lab2a/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2a
+{
+    class Card
+    {
+        // 1 Hearts
+        // 2 Clover
+        // 3 Diemond
+        // 4 Spades
+        int type;
+
+        // 0 Ace
+        // 1-10 number
+        // 11 jack 
+        // 12 queen
+        // 13 king
+        int kind;
+
+
+    }
+}
