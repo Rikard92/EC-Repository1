bb466f29fb37a20a7a9aa1bbad21e1af75d4b13f
Rikard92 <Rikard.Persson92@hotmail.com>
2016-03-08 09:45:47 +0100

    Lab2a Balckjack

---
+++ Lab2a/Lab2a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //skapa kort klass
+            //spapa deck klass
+            //skapa dealer klass
+        }
+    }
+}
